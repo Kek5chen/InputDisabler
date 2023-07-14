a41ebb5e73a7efdcc9370daec2c6aa43724781ee
Kek5chen <kx@imkx.dev>
2023-07-14 19:20:30 +0200

    i think i fixed it

--- content.js
+++ content.js
@@ -1,38 +1,29 @@
-const customBackgroundColor = 'rgb(250, 250, 250)'; 
+const customBackgroundColor = 'rgb(250, 250, 250)';
 
 let currentInputField = null;
-let keydownEventListener = null;
 
 document.addEventListener('mouseover', function (event) {
     if (event.target.tagName.toLowerCase() === 'input') {
         currentInputField = event.target;
-        
-        keydownEventListener = function (event) {
-            if (event.ctrlKey && event.key === 'q') {
-                event.preventDefault();
-                
-                if (currentInputField.disabled && currentInputField.style.backgroundColor === customBackgroundColor) {
-                    currentInputField.disabled = false;
-                    currentInputField.style.backgroundColor = '';
-                } else if (!currentInputField.disabled) {
-                    currentInputField.disabled = true;
-                    currentInputField.style.backgroundColor = customBackgroundColor;
-                }
-            }
-        };
-
-        document.addEventListener('keydown', keydownEventListener);
     }
 });
 
 document.addEventListener('mouseout', function (event) {
     if (event.target === currentInputField) {
         currentInputField = null;
-        
-        if (keydownEventListener) {
-            document.removeEventListener('keydown', keydownEventListener);
-            keydownEventListener = null;
-        }
     }
 });
 
+document.addEventListener('keydown', function (event) {
+    if (currentInputField && event.ctrlKey && event.key === 'q') {
+        event.preventDefault();
+
+        if (currentInputField.disabled && currentInputField.style.backgroundColor === customBackgroundColor) {
+            currentInputField.disabled = false;
+            currentInputField.style.backgroundColor = '';
+        } else if (!currentInputField.disabled) {
+            currentInputField.disabled = true;
+            currentInputField.style.backgroundColor = customBackgroundColor;
+        }
+    }
+});
